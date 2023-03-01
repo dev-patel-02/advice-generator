d2e31fc174363410eacfa7e2640eb4d9724518dc
Dev Patel <dev220791@gmail.com>
2023-03-01 17:56:00 +0600

    advice generator app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import AdviceCard from "./components/AdviceCard";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen px-10 container mx-auto">
+      <AdviceCard />
+    </div>
+  );
+}
+
+export default App;
